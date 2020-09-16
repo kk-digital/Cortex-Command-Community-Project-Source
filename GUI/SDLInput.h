2930f3b78d3ed4c8636b57a7d212eae1d38c4c40
Leo Goettlicher <lesgoets@gmail.com>
2020-09-16 22:19:50 +0200

    add SDL input class

---
+++ GUI/SDLInput.h
@@ -0,0 +1,24 @@
+#ifndef _RTESDLINPUT_
+#define _RTESDLINPUT_
+
+#include "GUIInput.h"
+#include <SDL2/SDL.h>
+
+namespace RTE {
+
+	class SDLInput : public GUIInput {
+	public:
+		SDLInput(int whichPlayer, bool keyJoyMouseCursor= false);
+
+		~SDLInput();
+
+		void Update() override;
+
+	private:
+		void ConvertKeyEvent(int sdlKey, int guilibKey, float elapsedS);
+
+		bool CreateTimer();
+
+		float GetTime();
+	};
+} // namespace RTE
