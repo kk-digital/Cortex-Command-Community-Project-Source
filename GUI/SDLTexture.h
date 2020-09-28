21fa9ad890759c3e1fef2bb4a44a20cd6def4f1c
Leo Goettlicher <lesgoets@gmail.com>
2020-09-28 20:41:08 +0200

    added more documentation and  pixel access functions

--- GUI/SDLTexture.h
+++ GUI/SDLTexture.h
@@ -4,30 +4,32 @@
 #include "GUI/Interface.h"
 #include "ContentFile.h"
 #include <SDL2/SDL.h>
+#include "RTEError.h"
 
 namespace RTE {
-	class SDLBitmap : public GUIBitmap {
+	class SDLTexture : public GUIBitmap {
 	public:
 		/// <summary>
 		/// Constructor method to instantiate a SDLBitmap
 		/// </summary>
-		SDLBitmap();
+		SDLTexture();
 
 		/// <summary>
 		/// Constructor to instantiate a SDLBitmap from an existing SDL_Texture
 		/// </summary>
 		/// <param name="pTexture">Pointer to a texture to instantiate from </param>
-		SDLBitmap(SDL_Texture *pSurface);
+		SDLTexture(SDL_Texture *pSurface);
 
 		/// <summary>
 		/// Destructor method to clean up the SDLBitmap object
 		/// </summary>
-		~SDLBitmap();
+		~SDLTexture();
 
 		/// <summary>
 		/// Create a new texture from a fileName
 		/// </summary>
 		/// <param name="filename">File to create texture from</param>
+		/// <returns> True if the Texture was successfully loaded</returns>
 		bool Create(const std::string filename);
 
 		/// <summary>
@@ -38,10 +40,11 @@ namespace RTE {
 		/// <param name="depth">
 		/// Unused, all textures will be created to match the screen colordepth
 		/// </param>
+		/// <returns> True if the texture was successfully created </returns>
 		bool Create(int width, int height, int depth = 8);
 
 		/// <summary>
-		/// Destroys and resets the SDLBitmap object.
+		/// Destroys and resets the SDLTextutre object.
 		/// </summary>
 		void Destroy() override;
 
@@ -95,14 +98,17 @@ namespace RTE {
 		                   bool filled) override;
 
 		/// <summary>
-		/// Get the color of the pixel at coordinates (x,y)
+		/// Get the color of the pixel at coordinates (x,y).
+		/// For this the texture needs to be locked.
 		/// </summary>
 		/// <param name="x">x coordinate</param>
 		/// <param name="y">y coordinate</param>
+		/// <returns> Color of the Pixel at coordinate (x,y)</returns>
 		unsigned long GetPixel(int x, int y) override;
 
 		/// <summary>
-		/// Set the Color of the pixel at coordinates (x,y)
+		/// Set the Color of the pixel at coordinates (x,y).
+		/// For this the texture needs to be locked.
 		/// </summary>
 		/// <param name="x">x coordinate</param>
 		/// <param name="y">y coordinate</param>
@@ -110,17 +116,48 @@ namespace RTE {
 		void SetPixel(int x, int y, unsigned long color) override;
 
 		/// <summary>
+		/// Lock the texture for direct pixel access
+		/// </summary>
+		/// <returns> true if the texture was locked successfully</returns>
+		bool LockTexture() { return LockTexture(NULL); }
+
+		/// <summary>
+		/// Lock the texture in the specified rectangle
+		/// </summary>
+		/// <param name="rect"> Rectangle inside the texture to lock</param>
+		bool LockTexture(SDL_Rect *rect);
+
+		/// <summary>
+		/// Unlock texture after editing pixels
+		/// </summary>
+		/// <returns> true if the texture was unlocked successfully</returns>
+		bool UnlockTexture();
+
+		/// <summary>
+		/// Get the pointer to the pixels array
+		/// </summary>
+		void *GetPixels() { return m_Pixels; }
+
+		/// <summary>
+		/// Get the length of a row of pixels in memory
+		/// </summary>
+		/// <returns>integer representing length of a row</returns>
+		int GetPitch() { return m_Pitch; }
+
+		/// <summary>
 		/// Get the width of the texture
 		/// </summary>
-		int GetWidth() override;
+		/// <returns> Width of the texture</returns>
+		int GetWidth() override { return m_Width; }
 
 		/// <summary>
 		/// Get the height of the texture
 		/// </summary>
-		int GetHeight() override;
+		/// <returns> Height of the texture </returns>
+		int GetHeight() override { return m_Height; }
 
 		/// <summary>
-		/// Get the color depth of the texture
+		/// Get the color depth of the texture (deprecated)
 		/// </summary>
 		int GetColorDepth() override;
 
@@ -146,6 +183,7 @@ namespace RTE {
 		/// <summary>
 		/// Get the path to the datafile object in use by this GUIBitmap
 		/// </summary>
+		/// <returns> The Path to the datafile </returns>
 		std::string GetDataPath() override;
 
 		/// <summary>
@@ -156,10 +194,11 @@ namespace RTE {
 		/// <summary>
 		/// Returns wether this SDLTexture has texture data.
 		/// </summary>
+		/// <returns> True if a texture is set</returns>
 		bool HasBitmap() override { return m_Texture != 0; }
 
 	private:
-		ContentFile m_BitmapFile;
+		ContentFile m_TextureFile;
 
 		SDL_Texture *m_Texture;
 		bool m_SelfCreated;
