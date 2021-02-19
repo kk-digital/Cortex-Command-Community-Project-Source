81a9c558acee994c410f0aae3ee512e660ed7983
HeliumAnt <lesgoets@gmail.com>
2021-02-19 05:48:30 +0100

    added temporary main test.cpp

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <SDL2/SDL.h>
+#include "Managers/SDLFrameMan.h"
+
+using namespace RTE;
+
+static bool quit = false;
+int quit_handler(void* quit, SDL_Event* event){
+	if((*event).type==SDL_QUIT){
+		*static_cast<int*>(quit) = true;
+	}
+	return 1;
+}
+
+int main(){
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_AddEventWatch(quit_handler, &quit);
+	g_FrameMan.Initialize();
+	SDL_Event e;
+	while(!quit){
+		SDL_PollEvent(&e);
+	}
+	g_FrameMan.Destroy();
+	SDL_Quit();
+}
