c3179e6d5af152d182f11065e189de8d36b9f3e4
HeliumAnt <lesgoets@gmail.com>
2021-02-19 05:14:34 +0100

    more sensible GUIRect definition compatible with how its actually used

--- GUI/GUI.h
+++ GUI/GUI.h
@@ -3,19 +3,21 @@
 
 #pragma region Rectangle Structure
 /// <summary>
-/// The GUIRect structure defines a rectangle by the coordinates of its upper-left and lower-right corners.
+/// The GUIRect structure defines a rectangle by the coordinates of its upper-left and lower-right corners as well as width and height.
 /// </summary>
-struct GUIRect { long left; long top; long right; long bottom; };
+struct GUIRect { int x; int y; int w; int h;};
 
 /// <summary>
 /// Sets the bounds of a GUIRect.
 /// </summary>
 /// <param name="pRect">Pointer to the GUIRect.</param>
-/// <param name="left">Position of top left corner on X axis.</param>
-/// <param name="top">Position of top left corner on Y axis.</param>
-/// <param name="right">Position of bottom right corner on X axis.</param>
-/// <param name="bottom">Position of bottom right corner on Y axis.</param>
-inline void SetRect(GUIRect *rect, int left, int top, int right, int bottom) { rect->left = left; rect->top = top; rect->right = right; rect->bottom = bottom; }
+/// <param name="x">Position of top left corner on X axis.</param>
+/// <param name="y">Position of top left corner on Y axis.</param>
+/// <param name="width">Width of the Rectangle<./param>
+/// <param name="height">Height of the Rectangle.</param>
+inline void SetRect(GUIRect &rect, int x, int y, int width, int height){
+	rect.x = x; rect.y = y; rect.w = width; rect.h = height;
+}
 #pragma endregion
 
 #include "RTETools.h"
