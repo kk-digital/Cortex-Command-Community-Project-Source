90dc04d153d1d16348eefb3d7d72a8ec00f85667
HeliumAnt <lesgoets@gmail.com>
2021-05-05 23:47:15 +0200

    converted to SDL

--- Managers/PrimitiveMan.cpp
+++ Managers/PrimitiveMan.cpp
@@ -9,16 +9,16 @@ namespace RTE {
 	void PrimitiveMan::DrawBitmapPrimitive(int player, const Vector &centerPos, Entity *entity, float rotAngle, int frame, bool hFlipped, bool vFlipped) {
 		const MOSprite *moSprite = dynamic_cast<MOSprite *>(entity);
 		if (moSprite) {
-			BITMAP *bitmap = moSprite->GetSpriteFrame(frame);
+			std::shared_ptr<Texture> bitmap = moSprite->GetSpriteFrame(frame);
 			if (bitmap) { m_ScheduledPrimitives.push_back(std::make_unique<BitmapPrimitive>(player, centerPos, bitmap, rotAngle, hFlipped, vFlipped)); }
 		}
 	}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	void PrimitiveMan::DrawPrimitives(int player, BITMAP *targetBitmap, const Vector &targetPos) const {
+	void PrimitiveMan::DrawPrimitives(int player, SDL_Renderer* renderer, const Vector &targetPos) const {
 		for (const std::unique_ptr<GraphicalPrimitive> &primitive : m_ScheduledPrimitives) {
-			if (primitive->m_Player == player || primitive->m_Player == -1) { primitive->Draw(targetBitmap, targetPos); }
+			if (primitive->m_Player == player || primitive->m_Player == -1) { primitive->Draw(renderer, targetPos); }
 		}
 	}
 }
