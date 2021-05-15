55a408c85a489fd4e4e85caad6a65c4d19aa3237
HeliumAnt <lesgoets@gmail.com>
2021-05-15 19:49:42 +0200

    move operators from header to cpp

--- System/SDLHelper.h
+++ System/SDLHelper.h
@@ -11,42 +11,28 @@
 /// <summary>
 /// Shift the Position of an SDL_Rect by an SDL_Point
 /// </summary>
-SDL_Rect operator+(const SDL_Rect &lhs, const SDL_Point &rhs) {
-	return SDL_Rect{lhs.x + rhs.x, lhs.y + rhs.y, lhs.w, lhs.h};
-}
+SDL_Rect operator+(const SDL_Rect &lhs, const SDL_Point &rhs);
 
-SDL_Rect operator*(const SDL_Rect &lhs, const int rhs){
-	return SDL_Rect{lhs.x, lhs.y, lhs.w*rhs, lhs.h*rhs};
-}
+SDL_Rect operator*(const SDL_Rect &lhs, const int rhs);
 
-SDL_Rect operator*(const SDL_Rect &lhs, const float rhs){
-	return lhs*static_cast<int>(rhs);
-}
+SDL_Rect operator*(const SDL_Rect &lhs, const float rhs);
 
-SDL_Rect operator*(const SDL_Rect &lhs, const double rhs){
-	return lhs*static_cast<int>(rhs);
-}
+SDL_Rect operator*(const SDL_Rect &lhs, const double rhs);
 
 /// <summary>
 /// Shift the Position of an SDL_Rect by -SDL_Point
 /// </summary>
-SDL_Rect operator-(const SDL_Rect &lhs, const SDL_Point &rhs) {
-	return SDL_Rect{lhs.x - rhs.x, lhs.y - rhs.y, lhs.w, lhs.h};
-}
+SDL_Rect operator-(const SDL_Rect &lhs, const SDL_Point &rhs);
 
 /// <summary>
 /// Add two SDL_Points
 /// </summary>
-SDL_Point operator+(const SDL_Point &lhs, const SDL_Point &rhs) {
-	return SDL_Point{lhs.x + rhs.x, lhs.x + rhs.y};
-}
+SDL_Point operator+(const SDL_Point &lhs, const SDL_Point &rhs);
 
 /// <summary>
 /// Subtract two SDL_Points
 /// </summary>
-SDL_Point operator-(const SDL_Point &lhs, const SDL_Point &rhs) {
-	return SDL_Point{lhs.x - rhs.x, lhs.y - rhs.x};
-}
+SDL_Point operator-(const SDL_Point &lhs, const SDL_Point &rhs);
 
 /// <summary>
 /// Deleter structure for the sdl pointers for use in smartpointers
