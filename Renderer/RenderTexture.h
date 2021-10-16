6db81a2c6af989dfab41e441a4e8fd7a8b905ffd
HeliumAnt <lesgoets@gmail.com>
2021-10-17 00:37:31 +0200

    extended the interface

--- Renderer/RenderTexture.h
+++ Renderer/RenderTexture.h
@@ -24,11 +24,18 @@ namespace RTE {
 		/// <returns>
 		/// True  if creation was successful.
 		/// </returns>
-		bool Create(int width, int height, uint32_t format);
+		void Create(int width, int height, uint32_t format = 0);
+
+		void Draw(RenderState& state) override;
+
+		void DrawClear() override;
+
+		std::shared_ptr<GLTexture> GetAsTexture() { return m_Texture; }
 
 	private:
+		using RenderTarget::Create;
 		std::shared_ptr<GLTexture> m_Texture; //!< Texture attachment to the framebuffer.
-		unsigned long m_FBO; //!< The framebuffer object that gets rendered to.
+		unsigned int m_FBO; //!< The framebuffer object that gets rendered to.
 	};
 }
 #endif
