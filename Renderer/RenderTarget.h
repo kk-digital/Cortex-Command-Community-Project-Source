b8a4b1686eaaa11723232648b73b1713fed0a6a9
HeliumAnt <lesgoets@gmail.com>
2021-10-19 22:08:28 +0200

    add documentation

--- Renderer/RenderTarget.h
+++ Renderer/RenderTarget.h
@@ -11,14 +11,35 @@ namespace RTE {
 
 		virtual void Create(int x, int y, int width, int height);
 
-		virtual void Draw(RenderState& state);
+		/// <summary>
+		/// Draws the passed state to the screen (or the active render target which should be the screen).
+		/// </summary>
+		/// <param name="state">
+		/// The render state that should be applied.
+		/// </param>
+		// TODO: States caching.
+		virtual void Draw(RenderState &state);
 
+		/// <summary>
+		/// Clear the current render target.
+		/// </summary>
 		virtual void DrawClear();
 
+		/// <summary>
+		/// Get the dimensions in pixels of the render target.
+		/// </summary>
+		/// <returns>
+		/// A vector containing the width and height of the render target.
+		/// </returns>
+		virtual glm::vec2 GetSize() { return m_Size; }
+
+	protected:
+		glm::mat4 m_Projection; //!< Orthographic projection matrix to transform screen coords to normalized screen coordinates.
+		glm::vec4 m_View; //!< Current view of this render target. //TODO: implement
+
+		glm::vec2 m_Size; //!< Size in pixels of this rendertarget.
 	private:
 		VertexArray m_DefaultQuad; //!< The default unit rectangle, used for drawing sprites without primitive attachments.
-		glm::mat4 m_Projection; //!< Orthographic projection matrix to transform screen coords to normalized screen coordinates.
-		glm::mat4 m_View; //!< Currently unused, because we implement the camera differently (badly).
 	};
-}
+} // namespace RTE
 #endif
--- Renderer/RenderTexture.h
+++ Renderer/RenderTexture.h
@@ -26,16 +26,46 @@ namespace RTE {
 		/// </returns>
 		void Create(int width, int height, uint32_t format = 0);
 
-		void Draw(RenderState& state) override;
+		/// <summary>
+		/// Draw the passed state to this render target.
+		/// </summary>
+		/// <param name="state">
+		/// The state to draw.
+		/// </param>
+		void Draw(RenderState &state) override;
 
+		/// <summary>
+		/// Clear this texture.
+		/// </summary>
 		void DrawClear() override;
 
+		/// <summary>
+		/// Get the texture attachment of this render target.
+		/// </summary>
+		/// <returns>
+		/// Shared pointer to the texture attachment.
+		/// </returns>
 		std::shared_ptr<GLTexture> GetAsTexture() { return m_Texture; }
+
+		/// <summary>
+		/// Get the width of the attached texture.
+		/// </summary>
+		/// <returns>
+		/// The width in pixels of the texture.
+		/// </returns>
+		int GetW() const;
+		/// <summary>
+		/// Get the height of the attached texture.
+		/// </summary>
+		/// <returns>
+		/// The height in pixels of the texture.
+		/// </returns>
+		int GetH() const;
 
 	private:
 		using RenderTarget::Create;
 		std::shared_ptr<GLTexture> m_Texture; //!< Texture attachment to the framebuffer.
 		unsigned int m_FBO; //!< The framebuffer object that gets rendered to.
 	};
-}
+} // namespace RTE
 #endif
