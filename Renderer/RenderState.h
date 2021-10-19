e6d2afa5411b1bb67929e63a12c41b80f82454ac
HeliumAnt <lesgoets@gmail.com>
2021-10-19 22:08:05 +0200

    add documentation

--- Renderer/RenderState.h
+++ Renderer/RenderState.h
@@ -13,13 +13,13 @@ namespace RTE {
 
 		RenderState(glm::vec4 color, std::shared_ptr<VertexArray> vertexArray, glm::mat4 modelTransform);
 
-		glm::vec4 m_Color;
-		glm::mat4 m_ModelTransform;
-		BlendMode m_BlendMode;
-		PrimitiveType m_PrimitiveType;
-		std::shared_ptr<Shader> m_Shader;
 		std::shared_ptr<VertexArray> m_Vertices; //!< Vertices to draw.
 		std::shared_ptr<GLTexture> m_Texture; //!< Texture to draw.
+		glm::vec4 m_Color; //!< Color modifier applied to the rteColorMod uniform.
+		glm::mat4 m_ModelTransform; //!< translation * rotate * scale in that order.
+		BlendMode m_BlendMode; //!< Blendmode to use for drawing. (applied after shader stage)
+		PrimitiveType m_PrimitiveType; //!< What primitive type to use for drawing the vertices.
+		std::shared_ptr<Shader> m_Shader; //!< Shader to use for drawing (must not be nullptr ever).
 	};
 
 }
