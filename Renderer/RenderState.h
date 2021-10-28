3626d38f5ce4d235d2d943b322c788333ec010b7
HeliumAnt <lesgoets@gmail.com>
2021-10-28 22:49:11 +0200

    don't use sharedptr because were usually passing this pointers

--- Renderer/RenderState.h
+++ Renderer/RenderState.h
@@ -6,15 +6,15 @@ namespace RTE {
 	class Shader;
 	class VertexArray;
 	class GLTexture;
-	struct RenderState{
+	struct RenderState {
 	public:
 		RenderState();
-		RenderState(std::shared_ptr<GLTexture> texture, glm::mat4 modelTransform, std::shared_ptr<Shader> shader, BlendMode blendMode, glm::vec4 colorMod);
+		RenderState(GLTexture *texture, glm::mat4 modelTransform, std::shared_ptr<Shader> shader, BlendMode blendMode, glm::vec4 colorMod);
 
-		RenderState(glm::vec4 color, std::shared_ptr<VertexArray> vertexArray, glm::mat4 modelTransform);
+		RenderState(glm::vec4 color, VertexArray *vertexArray, glm::mat4 modelTransform);
 
-		std::shared_ptr<VertexArray> m_Vertices; //!< Vertices to draw.
-		std::shared_ptr<GLTexture> m_Texture; //!< Texture to draw.
+		VertexArray *m_Vertices; //!< Vertices to draw.
+		GLTexture *m_Texture; //!< Texture to draw.
 		glm::vec4 m_Color; //!< Color modifier applied to the rteColorMod uniform.
 		glm::mat4 m_ModelTransform; //!< translation * rotate * scale in that order.
 		BlendMode m_BlendMode; //!< Blendmode to use for drawing. (applied after shader stage)
@@ -22,5 +22,5 @@ namespace RTE {
 		std::shared_ptr<Shader> m_Shader; //!< Shader to use for drawing (must not be nullptr ever).
 	};
 
-}
+} // namespace RTE
 #endif
