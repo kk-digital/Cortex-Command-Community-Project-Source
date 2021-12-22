eb740ccffe564e36945ba8627658f671e5cb0101
HeliumAnt <lesgoets@gmail.com>
2021-12-22 19:21:49 +0100

    set correct filtering otherwise wont be drawn

--- Renderer/GLPalette.cpp
+++ Renderer/GLPalette.cpp
@@ -6,9 +6,14 @@ namespace RTE {
 	Palette::Palette(const std::array<glm::u8vec4, PALETTESIZE> &data) :
 	    m_Palette(data) {
 		glCheck(glGenTextures(1, &m_Texture));
+		glCheck(glActiveTexture(GL_TEXTURE1));
 		glCheck(glBindTexture(GL_TEXTURE_1D, m_Texture));
+		glCheck(glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		glCheck(glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
 		glCheck(glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, PALETTESIZE, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_Palette.data()));
+		glCheck(glGenerateMipmap(GL_TEXTURE_1D));
 		glCheck(glBindTexture(GL_TEXTURE_1D, 0));
+		glCheck(glActiveTexture(GL_TEXTURE0));
 
 		m_SDLPalette = std::unique_ptr<SDL_Palette, sdl_palette_deleter>(SDL_AllocPalette(PALETTESIZE));
 		SDL_SetPaletteColors(m_SDLPalette.get(), static_cast<SDL_Color *>(static_cast<void *>(m_Palette.data())), 0, PALETTESIZE);
@@ -36,6 +41,7 @@ namespace RTE {
 	}
 
 	void Palette::Update() {
+		glCheck(glActiveTexture(GL_TEXTURE1));
 		glCheck(glBindTexture(GL_TEXTURE_1D, m_Texture));
 		glCheck(glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA, PALETTESIZE, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_Palette.data()));
 		glCheck(glBindTexture(GL_TEXTURE_1D, 0));
