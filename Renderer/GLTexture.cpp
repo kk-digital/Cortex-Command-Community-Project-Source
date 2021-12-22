4c5c17690034d5e89f98aa47a6c58e13ccd3bd06
HeliumAnt <lesgoets@gmail.com>
2021-12-22 19:22:38 +0100

    allow renderstate override

--- Renderer/GLTexture.cpp
+++ Renderer/GLTexture.cpp
@@ -12,11 +12,10 @@
 #include "GL/glew.h"
 #include "glm/gtc/matrix_transform.hpp"
 
-#include <SDL2/SDL.h>
+#include "Managers/FrameMan.h"
+
+#include "SDL2/SDL.h"
 namespace RTE {
-
-	void sdl_surface_deleter::operator()(SDL_Surface *p) { SDL_FreeSurface(p); }
-
 	GLTexture::GLTexture() :
 	    Surface() {
 		Clear();
@@ -39,13 +38,19 @@ namespace RTE {
 		}
 	}
 
-	bool GLTexture::Create(int width, int height) {
+	bool GLTexture::InitializeTextureObject(int width, int height) {
+		glCheck(glActiveTexture(GL_TEXTURE0));
 		glCheck(glBindTexture(GL_TEXTURE_2D, m_TextureID));
 		glCheck(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
 		glCheck(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		glCheck(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+		glCheck(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
 
 		glCheck(glTexImage2D(GL_TEXTURE_2D, 0, m_BPP == 8 ? GL_RED : GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_INT, nullptr));
 		glCheck(glBindTexture(GL_TEXTURE_2D, 0));
+
+		m_ShaderBase = g_FrameMan.GetTextureShader(m_BPP == 8 ? BitDepth::Indexed8 : BitDepth::BPP32);
+		m_ShaderBase = g_FrameMan.GetTextureShaderFill(m_BPP == 8 ? BitDepth::Indexed8 : BitDepth::BPP32);
 
 		return true;
 	}
@@ -55,9 +60,11 @@ namespace RTE {
 		m_Width = width;
 		m_Height = height;
 		m_BPP = format == BitDepth::BPP32 ? 32 : 8;
-		if (Create(width, height)) {
+		if (InitializeTextureObject(width, height)) {
 			if (Surface::Create(width, height, format, palette)) {
 				Update();
+				m_ShaderBase = g_FrameMan.GetTextureShader(m_BPP == 8 ? BitDepth::Indexed8 : BitDepth::BPP32);
+				m_ShaderBase = g_FrameMan.GetTextureShaderFill(m_BPP == 8 ? BitDepth::Indexed8 : BitDepth::BPP32);
 				return true;
 			}
 		}
@@ -76,55 +83,107 @@ namespace RTE {
 		m_ShaderFill = nullptr;
 		m_ShaderCustom = nullptr;
 	}
-	void GLTexture::render(RenderTarget *renderer, float x, float y) {
-		render(renderer, {x, y});
+
+	void GLTexture::render(RenderTarget *renderer, float x, float y, std::optional<RenderState> state) {
+		render(renderer, {x, y}, state);
 	}
-	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos) {
+	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, std::optional<RenderState> state) {
 		glm::mat4 translation = glm::translate(glm::mat4(1.0f), {pos, 0.0f});
+		translation = glm::scale(translation, glm::vec3(GetSize(), 1.0f));
 
 		RenderState render{shared_from_this(), translation, GetCurrentShader(), m_BlendMode, m_ColorMod};
+		if (state) {
+			if (state->m_Vertices) {
+				render.m_Vertices = state->m_Vertices;
+			}
+			if (state->m_Shader) {
+				render.m_Shader = state->m_Shader;
+			}
+			render.m_PrimitiveType = state->m_PrimitiveType;
+			render.m_FBO = state->m_FBO;
+		}
 
 		renderer->Draw(render);
 	}
-	void GLTexture::render(RenderTarget *renderer, float x, float y, float angle) {
-		render(renderer, {x, y}, angle);
+	void GLTexture::render(RenderTarget *renderer, float x, float y, float angle, std::optional<RenderState> state) {
+		render(renderer, {x, y}, angle, state);
 	}
-	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, float angle) {
+	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, float angle, std::optional<RenderState> state) {
 		glm::vec2 size = GetSize();
 		glm::mat4 modelTransform = glm::translate(glm::mat4(1.0f), {pos, 0.0f});
 		modelTransform = glm::rotate(modelTransform, glm::radians(angle), {0.0f, 0.0f, 1.0f});
 		modelTransform = glm::translate(modelTransform, {-0.5f * size, 0.0f});
 		modelTransform = glm::scale(modelTransform, {size, 1.0f});
 		RenderState render(shared_from_this(), modelTransform, GetCurrentShader(), m_BlendMode, m_ColorMod);
+		if (state) {
+			if (state->m_Vertices) {
+				render.m_Vertices = state->m_Vertices;
+			}
+			if (state->m_Shader) {
+				render.m_Shader = state->m_Shader;
+			}
+			render.m_PrimitiveType = state->m_PrimitiveType;
+			render.m_FBO = state->m_FBO;
+		}
 		renderer->Draw(render);
 	}
-	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, glm::vec2 scale) {
+	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, glm::vec2 scale, std::optional<RenderState> state) {
 		glm::vec2 size = GetSize();
 		glm::mat4 modelTransform = glm::translate(glm::mat4(1.0f), {pos, 0.0f});
 		modelTransform = glm::scale(modelTransform, {scale * size, 1.0f});
 		RenderState render(shared_from_this(), modelTransform, GetCurrentShader(), m_BlendMode, m_ColorMod);
+		if (state) {
+			if (state->m_Vertices) {
+				render.m_Vertices = state->m_Vertices;
+			}
+			if (state->m_Shader) {
+				render.m_Shader = state->m_Shader;
+			}
+			render.m_PrimitiveType = state->m_PrimitiveType;
+			render.m_FBO = state->m_FBO;
+		}
 		renderer->Draw(render);
 	}
-	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, float angle, glm::vec2 scale) {
+	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, float angle, glm::vec2 scale, std::optional<RenderState> state) {
 		glm::vec2 size = GetSize();
 		glm::mat4 modelTransform = glm::translate(glm::mat4(1.0f), {pos, 0.0f});
 		modelTransform = glm::rotate(modelTransform, glm::radians(angle), {0.0f, 0.0f, 1.0f});
 		modelTransform = glm::translate(modelTransform, {-0.5f * size, 0.0f});
 		modelTransform = glm::scale(modelTransform, {scale * size, 1.0f});
 		RenderState render(shared_from_this(), modelTransform, GetCurrentShader(), m_BlendMode, m_ColorMod);
+		if (state) {
+			if (state->m_Vertices) {
+				render.m_Vertices = state->m_Vertices;
+			}
+			if (state->m_Shader) {
+				render.m_Shader = state->m_Shader;
+			}
+			render.m_PrimitiveType = state->m_PrimitiveType;
+			render.m_FBO = state->m_FBO;
+		}
 		renderer->Draw(render);
 	}
 
-	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, float angle, glm::vec2 center, glm::vec2 scale) {
+	void GLTexture::render(RenderTarget *renderer, glm::vec2 pos, float angle, glm::vec2 center, glm::vec2 scale, std::optional<RenderState> state) {
 		glm::mat4 modelTransform = glm::translate(glm::mat4(1.0f), {pos, 0.0f});
 		modelTransform = glm::rotate(modelTransform, glm::radians(angle), {0.0f, 0.0f, 1.0f});
 		modelTransform = glm::translate(modelTransform, {center, 0.0f});
 		modelTransform = glm::scale(modelTransform, {scale * GetSize(), 1.0f});
 		RenderState render(shared_from_this(), modelTransform, GetCurrentShader(), m_BlendMode, m_ColorMod);
+		if (state) {
+			if (state->m_Vertices) {
+				render.m_Vertices = state->m_Vertices;
+			}
+			if (state->m_Shader) {
+				render.m_Shader = state->m_Shader;
+			}
+			render.m_PrimitiveType = state->m_PrimitiveType;
+			render.m_FBO = state->m_FBO;
+		}
 		renderer->Draw(render);
 	}
 
-	void GLTexture::render(RenderTarget *renderer, glm::vec4 src, glm::vec4 dest) {
+	void GLTexture::render(RenderTarget *renderer, glm::vec4 src, glm::vec4 dest, std::optional<RenderState> state) {
 		std::vector<Vertex> uvQuad;
 		uvQuad.emplace_back(0.0f, 0.0f, src.x / m_Width, src.y / m_Height);
 		uvQuad.emplace_back(0.0f, 1.0f, src.x / m_Width, (src.y + src.w) / m_Height);
@@ -136,20 +195,29 @@ namespace RTE {
 		modelTransform = glm::scale(modelTransform, {dest.zw(), 1.0f});
 
 		RenderState render(shared_from_this(), modelTransform, GetCurrentShader(), m_BlendMode, m_ColorMod);
+		if (state) {
+			if (state->m_Vertices) {
+				render.m_Vertices = state->m_Vertices;
+			}
+			if (state->m_Shader) {
+				render.m_Shader = state->m_Shader;
+			}
+			render.m_PrimitiveType = state->m_PrimitiveType;
+			render.m_FBO = state->m_FBO;
+		}
 		render.m_Vertices = uvVBO;
 
 		renderer->Draw(render);
 	}
 
-	void GLTexture::render(RenderTarget *renderer, glm::vec4 dest) {
-		render(renderer, dest.xy(), {dest.z / GetW(), dest.w / GetH()});
+	void GLTexture::render(RenderTarget *renderer, glm::vec4 dest, std::optional<RenderState> state) {
+		render(renderer, dest.xy(), {dest.z / GetW(), dest.w / GetH()}, state);
 	}
 
 	void GLTexture::Update(std::optional<glm::vec4> region) {
 		if (region) {}
 
-		glCheck(glBindTexture(GL_TEXTURE_2D, m_TextureID));
-
+		Bind();
 		glCheck(glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, m_Width, m_Height, m_BPP == 8 ? GL_RED : GL_BGRA, GL_UNSIGNED_BYTE, GetPixels()->pixels));
 	}
 
@@ -168,8 +236,7 @@ namespace RTE {
 
 	void GLTexture::ClearColor(uint32_t color) {
 		Surface::ClearColor(color);
-		glCheck(glBindTexture(GL_TEXTURE_2D, m_TextureID));
-		glCheck(glClearTexImage(GL_TEXTURE_2D, 0, m_BPP == 8 ? GL_RED : GL_BGRA, m_BPP == 8 ? GL_UNSIGNED_BYTE : GL_UNSIGNED_INT_8_8_8_8, &color));
+		Update();
 	}
 
 	void GLTexture::Bind() {
--- Renderer/GLTexture.h
+++ Renderer/GLTexture.h
@@ -3,6 +3,7 @@
 
 #include "BlendMode.h"
 #include "Surface.h"
+#include "RenderState.h"
 
 namespace RTE {
 	class Shader;
@@ -24,26 +25,24 @@ namespace RTE {
 	public:
 		virtual ~GLTexture();
 
+		virtual bool Create(int width, int height, BitDepth format = BitDepth::Indexed8, std::optional<std::shared_ptr<Palette>> palette = std::nullopt) override;
 
-		virtual bool Create(int width, int height);
-		virtual bool Create(int width, int height, BitDepth format, std::optional<std::shared_ptr<Palette>> palette = std::nullopt) override;
+		void render(RenderTarget *renderer, float x, float y, std::optional<RenderState> state = std::nullopt);
 
-		void render(RenderTarget *renderer, float x, float y);
+		void render(RenderTarget *renderer, glm::vec2 pos, std::optional<RenderState> state = std::nullopt);
 
-		void render(RenderTarget *renderer, glm::vec2 pos);
+		void render(RenderTarget *renderer, float x, float y, float angle, std::optional<RenderState> state = std::nullopt);
 
-		void render(RenderTarget *renderer, float x, float y, float angle);
+		void render(RenderTarget *renderer, glm::vec2 pos, float angle, std::optional<RenderState> state = std::nullopt);
 
-		void render(RenderTarget *renderer, glm::vec2 pos, float angle);
+		void render(RenderTarget *renderer, glm::vec2 pos, glm::vec2 scale, std::optional<RenderState> state = std::nullopt);
 
-		void render(RenderTarget *renderer, glm::vec2 pos, glm::vec2 scale);
+		void render(RenderTarget *renderer, glm::vec2 pos, float angle, glm::vec2 scale, std::optional<RenderState> state = std::nullopt);
 
-		void render(RenderTarget *renderer, glm::vec2 pos, float angle, glm::vec2 scale);
+		void render(RenderTarget *renderer, glm::vec2 pos, float angle, glm::vec2 center, glm::vec2 scale, std::optional<RenderState> state = std::nullopt);
 
-		void render(RenderTarget *renderer, glm::vec2 pos, float angle, glm::vec2 center, glm::vec2 scale);
-
-		void render(RenderTarget *renderer, glm::vec4 src, glm::vec4 dest);
-		void render(RenderTarget *renderer, glm::vec4 dest);
+		void render(RenderTarget *renderer, glm::vec4 src, glm::vec4 dest, std::optional<RenderState> state = std::nullopt);
+		void render(RenderTarget *renderer, glm::vec4 dest, std::optional<RenderState> state = std::nullopt);
 
 		void Update(std::optional<glm::vec4> region = std::nullopt);
 
@@ -75,7 +74,7 @@ namespace RTE {
 
 		unsigned int GetTextureID() { return m_TextureID; }
 
-		bool SaveToDisk(const std::string& filename);
+		bool SaveToDisk(const std::string &filename);
 
 	private:
 		using Surface::Create;
@@ -99,9 +98,10 @@ namespace RTE {
 	private:
 		GLTexture();
 		GLTexture(std::shared_ptr<GLTexture> ref);
+		bool InitializeTextureObject(int width, int height);
 
-		template<class ...Args>
-		friend std::unique_ptr<GLTexture> MakeTexture(Args&& ...args) {
+		template <class... Args>
+		friend std::unique_ptr<GLTexture> MakeTexture(Args &&...args) {
 			return std::unique_ptr<GLTexture>(new GLTexture(std::forward<Args>(args)...));
 		}
 
@@ -112,7 +112,7 @@ namespace RTE {
 	/// <summary>
 	/// Factory function for creating smart pointers of GLTexture.
 	/// </summary>
-	template<class ...Args>
-	std::unique_ptr<GLTexture> MakeTexture(Args&& ...args);
+	template <class... Args>
+	std::unique_ptr<GLTexture> MakeTexture(Args &&...args);
 } // namespace RTE
 #endif
