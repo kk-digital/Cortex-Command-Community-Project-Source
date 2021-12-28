1b63cd4bebe1422e241057f70ca459d9e0b78ca2
HeliumAnt <lesgoets@gmail.com>
2021-12-28 02:30:04 +0100

    init to white instead of transparent black

--- Renderer/Vertex.cpp
+++ Renderer/Vertex.cpp
@@ -2,14 +2,14 @@
 
 namespace RTE {
 	Vertex::Vertex() :
-	    pos(0, 0), texUV(0, 0), color(0) {}
+	    pos(0, 0), texUV(0, 0), color(255) {}
 	Vertex::~Vertex() {}
 
 	Vertex::Vertex(glm::vec2 position) :
-	    pos(position), texUV(0, 0), color(0) {}
+	    pos(position), texUV(0, 0), color(255) {}
 
 	Vertex::Vertex(glm::vec2 position, glm::vec2 textureUV) :
-	    pos(position), texUV(textureUV), color(0) {}
+	    pos(position), texUV(textureUV), color(255) {}
 
 	Vertex::Vertex(glm::vec2 position, glm::u8vec4 color) :
 	    pos(position), texUV(0, 0), color(color) {}
@@ -18,10 +18,10 @@ namespace RTE {
 	    pos(position), texUV(0, 0), color((color >> 16) & 0xFF, (color >> 8) & 0xff, (color)&0xff, (color >> 24) & 0xFF) {}
 
 	Vertex::Vertex(float x, float y) :
-	    pos(x, y), texUV(0), color(0) {}
+	    pos(x, y), texUV(0), color(255) {}
 
 	Vertex::Vertex(float x, float y, float u, float v) :
-	    pos(x, y), texUV(u, v), color(0) {}
+	    pos(x, y), texUV(u, v), color(255) {}
 
 	Vertex::Vertex(float x, float y, glm::u8vec4 color) :
 	    pos(x, y), color(color) {}
